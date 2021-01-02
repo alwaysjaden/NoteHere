48bd94fd67e160507a42d81dd7a1c389e82fee8f
Jaden Lee <alwaysjaden@gmail.com>
2021-01-02 00:49:16 -0800

    port

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ console.log(database)
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
   });
   
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
   });
 
   
